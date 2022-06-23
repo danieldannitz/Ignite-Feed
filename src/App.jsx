c1f64fbd7c39acd90e0b1c26f3caa89cd1ff57cd
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-23 12:47:13 -0300

    app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
 
+import styles from "./App.module.css";
 import "./global.css";
+import { SideBar } from "./components/SideBar";
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post
+            author="Daniel Menezes"
+            content="lorem Ipsum dolor sit amet, consectetur adipiscing elit lorem Ipsum dolor sit amet, consectetur adipiscing elitlorem Ipsum dolor sit amet, consectetur adipiscing elitlorem Ipsum dolor sit amet, consectetur adipiscing elit"
+          />
+          <Post author="Ronaldinho Gaúcho" content="novo texto" />
+        </main>
+      </div>
     </div>
   );
 }
