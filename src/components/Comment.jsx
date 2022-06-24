1992160792309f8ab3c0da0273ff9c34075a3244
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-24 12:30:08 -0300

    comment component done

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -15,7 +15,7 @@ export function Comment() {
               </time>
             </div>
             <button title="">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Carpincho, parabéns!! 👏👏</p>
