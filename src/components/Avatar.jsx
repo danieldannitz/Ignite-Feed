40fdcbdb4dc27d8acb06daaffbdeb14bb87d5f7d
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-25 10:23:13 -0300

    started avatar component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,10 @@
+import styles from "./Avatar.modules.css";
+
+export function Avatar() {
+  return (
+    <img
+      className={styles.avatar}
+      src="https://i.pinimg.com/736x/04/a5/1e/04a51e3a2263f59b0b71d9fac9ab2ae0--animal-portraits-portrait-art.jpg"
+    />
+  );
+}
