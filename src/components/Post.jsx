883c78ab17545bb85ffc3d067f37075b18263f16
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-27 16:57:25 -0300

    concept of react states

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,22 @@
-import { format, formatDistanceToNow } from "date-fns";
-import ptBR from "date-fns/esm/locale/pt-BR/index.js";
+import { useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/esm/locale/pt-BR/index.js";
+
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2]); //useState vai receber o array de comments, setComments fica responsável por atualizar comments quando o mesmo for alterado
+
+  function handleNewComment() {
+    event.preventDefault(); //previne a atualização por padrão do react
+
+    setComments([...comments, comments.length + 1]);
+  }
+
   const dateFormat = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
@@ -43,15 +53,15 @@ export function Post({ author, publishedAt, content }) {
           }
         })}
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário" />
         <button type="submit">Publicar</button>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
