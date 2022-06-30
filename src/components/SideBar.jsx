c78608e92c642be77b82e61537507dd9674fcc5b
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-30 15:03:04 -0300

    sidebar fix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,9 +10,9 @@ export function SideBar() {
         src="https://images.unsplash.com/photo-1631815087668-d039e9ead82f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <Avatar src="https://github.com/danieldannitz.png" />
-        <strong>Daniel Menezes</strong>
-        <span>Front-end Dev</span>
+        <Avatar src="https://i.pinimg.com/736x/04/a5/1e/04a51e3a2263f59b0b71d9fac9ab2ae0--animal-portraits-portrait-art.jpg" />
+        <strong>Carpincho da Silva</strong>
+        <span>Nadador</span>
       </div>
       <footer>
         <a href="#">
